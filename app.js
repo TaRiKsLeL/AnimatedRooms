a8cf7aa824028569118c623336f6ab519066b3c0
Taras <tariksyurts@gmail.com>
2020-06-04 03:48:26 +0300

    worktable

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ let roomObjects = $('.room-objects').children();
 
 let i = 0;
 let interval = setInterval(function() {
-    console.log($('.room-objects').eq(i));
-    roomObjects.eq(i).addClass('showed');
+    roomObjects.eq(i).addClass('animate__fadeInLeft');
     i++;
     if( i >= roomObjects.length ) {
         clearInterval(interval);
     }
-}, 1500);
+}, 1000);
+
+$('.worktable-slick').slick({
+    // arrows: false,
+    // dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
